6da1a554dcff9570e2dc6bb5735ac51c6a1876d1
Lubyanoy Ivan <pythonblack5000@gmail.com>
2023-04-04 16:39:14 +0300

    [#7] Pumpkin soup

--- src/main/java/gg/foodanddrinks/proxy/CommonProxy.java
+++ src/main/java/gg/foodanddrinks/proxy/CommonProxy.java
@@ -82,6 +82,7 @@ public class CommonProxy {
                 modreg.registerItem("melon_juice", new ItemContainedProduct(Type.DRINK, 5, 0.3F), true),
                 modreg.registerItem("pear", new ItemFood(3, 0.3F, false), true),
                 modreg.registerItem("pear_juice", new ItemContainedProduct(Type.DRINK, 6, 0.3F), true),
+                modreg.registerItem("pumpkin_soup", new ItemFood(4, 0.3F, false), true),
                 modreg.registerItem("sunflower_oil", new ItemContainedProduct(Type.DRINK).setBadFood(), true),
                 modreg.registerItem("totem_of_undying_extract", new ItemCustomPotion(Misc.getEffect(Misc.Effect.TOTEM_EXTRACT)), true),
                 modreg.registerItem("uncooked_fried_eggs", new ItemContainedProduct(Type.FOOD).setBadFood(), true)
